7738d4d7d36c71b7b0c7864209d33e2aa487e5fc
ropellanda <64418819+ropellanda@users.noreply.github.com>
2023-10-20 16:35:59 -0300

    Create install_utils.sh

---
+++ install_utils.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/BYOBMO/BMO-Utils.git
+sudo cp -r /home/pi/BMO-Utils/RetroPie/splashscreens/moco.png /home/pi/RetroPie/splashscreens
+sudo cp -r /home/pi/BMO-Utils/RetroPie/bmos-theme/ /etc/emulationstation/themes/
+cp /home/pi/BMO-Utils/pi/bashrc ~/.bashrc
+pip install SpeechRecognition
+cd home/pi/BMO-Utils/voice
+mkdir build
+cd build
+cmake ..
+make install
